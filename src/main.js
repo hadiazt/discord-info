11b99ff886a2aa484ddc3b07cd027523742dd380
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 00:48:49 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -18,6 +18,10 @@ exports.GUILD = ({ url }) => {
             // BANNER
             if (req.guild.banner !== null) req.guild.banner = { png: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.png?size=2048', jpg: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.jpg?size=2048', jpeg: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.jpeg?size=2048', webp: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.webp?size=2048', gif: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.gif?size=2048', }; else req.guild.banner = { png: null, jpg: null, jpeg: null, webp: null, gif: null, };
 
+            // ICON
+            if (req.guild.icon !== null) req.guild.icon = { png: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.png?size=2048', jpg: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.jpg?size=2048', jpeg: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.jpeg?size=2048', webp: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.webp?size=2048', gif: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.gif?size=2048', }; else req.guild.icon = { png: null, jpg: null, jpeg: null, webp: null, gif: null, }
+
+
             resolve({
                 inv: { invCode: req.code, invType: req.type, invExpireTime: req.expires_at, Channel: { id: req.channel.id, name: req.channel.name, type: req.channel.type } },
                 guild: { id: req.guild.id, name: req.guild.name, memberCount: req.approximate_member_count, presenceCount: req.approximate_presence_count, description: req.guild.description, splash: req.guild.splash, banner: req.guild.banner, icon: req.guild.icon, verificationLVL: req.guild.verification_level, vanityURL: req.guild.vanity_url_code, boostCount: req.guild.premium_subscription_count, nsfw: { NSFW: req.guild.nsfw, NSFWlvl: req.guild.nsfw_level }, wlcScreen: req.guild.welcome_screen, features: req.guild.features }
