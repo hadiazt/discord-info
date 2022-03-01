08f6392bf1c5cdaabefef1c03d522078d477a8ee
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 00:49:31 +0330

    Compressed The Code

--- src/main.js
+++ src/main.js
@@ -22,17 +22,8 @@ exports.GUILD = ({ url }) => {
             if (req.guild.icon !== null) req.guild.icon = { png: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.png?size=2048', jpg: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.jpg?size=2048', jpeg: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.jpeg?size=2048', webp: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.webp?size=2048', gif: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.gif?size=2048', }; else req.guild.icon = { png: null, jpg: null, jpeg: null, webp: null, gif: null, }
 
             // WLC SCREEN
-            if (req.guild.welcome_screen) req.guild.welcome_screen = {
-                description: req.guild.welcome_screen.description,
-                WLCChannels: req.guild.welcome_screen.welcome_channels,
-            }; else req.guild.welcome_screen = {
-                description: null,
-                WLCChannels: null
-            }
-            resolve({
-                inv: { invCode: req.code, invType: req.type, invExpireTime: req.expires_at, Channel: { id: req.channel.id, name: req.channel.name, type: req.channel.type } },
-                guild: { id: req.guild.id, name: req.guild.name, memberCount: req.approximate_member_count, presenceCount: req.approximate_presence_count, description: req.guild.description, splash: req.guild.splash, banner: req.guild.banner, icon: req.guild.icon, verificationLVL: req.guild.verification_level, vanityURL: req.guild.vanity_url_code, boostCount: req.guild.premium_subscription_count, nsfw: { NSFW: req.guild.nsfw, NSFWlvl: req.guild.nsfw_level }, wlcScreen: req.guild.welcome_screen, features: req.guild.features }
-            })
+            if (req.guild.welcome_screen) req.guild.welcome_screen = { description: req.guild.welcome_screen.description, WLCChannels: req.guild.welcome_screen.welcome_channels, }; else req.guild.welcome_scree = { description: null, WLCChannels: null }
+
 
         }).catch(reject);
     });
