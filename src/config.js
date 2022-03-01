8154cc67c550ec83968617ebf39ac66257300654
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 00:50:23 +0330

    Create config.js

---
+++ src/config.js
@@ -0,0 +1,11 @@
+exports.Settings = {
+    Guild: {
+        API: "https://discord.com/api/v10/invites/",
+        SPLASH: "https://cdn.discordapp.com/splashes/",
+        BANNER: "https://cdn.discordapp.com/banner/",
+        ICON: "https://cdn.discordapp.com/icons/"
+    },
+    User: {
+        API : "https://discord.com/api/v10/users/",
+    }
+}
