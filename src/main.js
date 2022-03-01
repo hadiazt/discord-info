d97cfd3826b7d512a8937c15b598d1d70d2b187f
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-01 23:50:07 +0330

    Added `INV` Info

--- src/main.js
+++ src/main.js
@@ -11,6 +11,13 @@ exports.GUILD = ({ url }) => {
     return new Promise((resolve, reject) => {
         axios(config).then(data => {
             const req = data.data;
+            resolve({
+                code: {
+                    invCode: req.code,
+                    invType: req.type,
+                    invExpireTime: req.expires_at
+                },
+            })
         }).catch(reject);
     });
 
