b5ff7305b9ac45246ba8260f3b9ad75578f1939f
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-03 00:35:36 +0330

    Added `AccountGuilds`

--- src/handler.js
+++ src/handler.js
@@ -1,8 +1,8 @@
-exports.Invite = require('./components/Invite.module');
-exports.User = require('./components/User.module');
-exports.Guild = require('./components/Guild.module');
-exports.GuildRoles = require('./components/GuildRoles.module');
-exports.GuildEmojis = require('./components/GuildEmojis.module');
-exports.Account = require('./components/Account.module')
-exports.AccountSettings = require('./components/AccountSettings.module');
-
+exports.Invite = require('./Components/Invite.module');
+exports.User = require('./Components/Bot/User.module');
+exports.Guild = require('./Components/Bot/Guild.module');
+exports.GuildRoles = require('./Components/Bot/GuildRoles.module');
+exports.GuildEmojis = require('./Components/Bot/GuildEmojis.module');
+exports.Account = require('./Components/UserAccount/Account.module')
+exports.AccountSettings = require('./Components/UserAccount/AccountSettings.module');
+exports.AccountGuilds = require('./Components/UserAccount/AccountGuilds.module.js');
