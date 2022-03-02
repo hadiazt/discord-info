4fb30c6608bd3959387b0b689bb51a8d3ceee8ae
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 22:47:38 +0330

    Added `handler.js`

---
+++ src/handler.js
@@ -0,0 +1,5 @@
+exports.Invite = require('./components/Invite.module');
+exports.User = require('./components/User.module');
+exports.Guild = require('./components/Guild.module');
+exports.GuildRoles = require('./components/GuildRoles.module')
+exports.GuildEmojis = require('./components/GuildEmojis.module')
