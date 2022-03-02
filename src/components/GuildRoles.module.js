fc313f96267f15895769e6d7254112f7789b3b90
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 22:49:26 +0330

    Create GuildRoles.module.js

---
+++ src/components/GuildRoles.module.js
@@ -0,0 +1,19 @@
+const axios = require('axios');
+const { Settings } = require('../config');
+
+module.exports = ({ GuildID, token }) => {
+    config = {
+        method: 'get',
+        url: Settings.Guild.API + GuildID,
+        headers: {
+            'Authorization': 'Bot ' + token,
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        axios(config).then(data => {
+            const roles = data.data.roles;
+            resolve(roles)
+  }).catch(e => { console.log(e?.response?.data?.message || e) });
+    });
+}
