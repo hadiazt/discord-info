3ff814d2826b5fbb019a39c62bd8a48cf6fa6ca7
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-03 00:35:43 +0330

    Create AccountSettings.module.js

---
+++ src/Components/UserAccount/AccountSettings.module.js
@@ -0,0 +1,22 @@
+const axios = require('axios');
+const { Settings } = require('../../config');
+
+module.exports = ({ UserToken }) => {
+
+    const config = {
+        method: 'get',
+        url: Settings.Account.API.SETTINGS,
+        headers: {
+            Authorization: UserToken,
+        }
+    };
+
+    return new Promise((resolve, reject) => {
+        axios(config).then(data => {
+            const req = data.data;
+            resolve(req)
+        }).catch(e => { console.log(e?.response?.data?.message || e) });
+    });
+}
+
+
