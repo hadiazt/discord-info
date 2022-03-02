579b2ffda655f54578c08788b912ecf6fbfe1fff
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 15:38:28 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -128,3 +128,19 @@ exports.GuildRoles = ({ GuildID, token }) => {
     });
 }
 
+exports.GuildEmojis = ({ GuildID, token }) => {
+    config = {
+        method: 'get',
+        url: Settings.Guild.API + GuildID,
+        headers: {
+            'Authorization': 'Bot ' + token,
+        }
+    }
+
+    return new Promise((resolve, reject) => {
+        axios(config).then(data => {
+            const emojis = data.data.emojis;
+        }).catch(e => { console.log(e) });
+    });
+}
+
