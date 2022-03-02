9c57c494d0b00b4dc864112cc5e7ddcf652367ba
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 14:20:31 +0330

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,19 +2,19 @@ const axios = require('axios');
 
 const { Settings } = require('./config');
 
-exports.GUILD = ({ url }) => {
+exports.Invite = ({ url }) => {
     config = {
         method: 'get',
-        url: Settings.Guild.API + url + '?with_counts=true',
+        url: Settings.Invite.API + url + '?with_counts=true',
     }
 
     return new Promise((resolve, reject) => {
         axios(config).then(data => {
             const req = data.data;
-            
-            if (req.guild.splash !== null) req.guild.splash = Settings.Guild.SPLASH + req.guild.id + '/' + req.guild.splash + '.png?size=2048'; else req.guild.splash = null
-            if (req.guild.banner !== null) req.guild.banner = { png: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.png?size=2048', jpg: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.jpg?size=2048', jpeg: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.jpeg?size=2048', webp: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.webp?size=2048', gif: Settings.Guild.BANNER + req.guild.id + '/' + req.guild.banner + '.gif?size=2048', }; else req.guild.banner = { png: null, jpg: null, jpeg: null, webp: null, gif: null, };
-            if (req.guild.icon !== null) req.guild.icon = { png: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.png?size=2048', jpg: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.jpg?size=2048', jpeg: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.jpeg?size=2048', webp: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.webp?size=2048', gif: Settings.Guild.ICON + req.guild.id + '/' + req.guild.icon + '.gif?size=2048', }; else req.guild.icon = { png: null, jpg: null, jpeg: null, webp: null, gif: null, }
+
+            if (req.guild.splash !== null) req.guild.splash = Settings.Invite.SPLASH + req.guild.id + '/' + req.guild.splash + '.png?size=2048'; else req.guild.splash = null
+            if (req.guild.banner !== null) req.guild.banner = { png: Settings.Invite.BANNER + req.guild.id + '/' + req.guild.banner + '.png?size=2048', jpg: Settings.Invite.BANNER + req.guild.id + '/' + req.guild.banner + '.jpg?size=2048', jpeg: Settings.Invite.BANNER + req.guild.id + '/' + req.guild.banner + '.jpeg?size=2048', webp: Settings.Invite.BANNER + req.guild.id + '/' + req.guild.banner + '.webp?size=2048', gif: Settings.Invite.BANNER + req.guild.id + '/' + req.guild.banner + '.gif?size=2048', }; else req.guild.banner = { png: null, jpg: null, jpeg: null, webp: null, gif: null, };
+            if (req.guild.icon !== null) req.guild.icon = { png: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.png?size=2048', jpg: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.jpg?size=2048', jpeg: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.jpeg?size=2048', webp: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.webp?size=2048', gif: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.gif?size=2048', }; else req.guild.icon = { png: null, jpg: null, jpeg: null, webp: null, gif: null, }
             if (req.guild.welcome_screen) req.guild.welcome_screen = { description: req.guild.welcome_screen.description, WLCChannels: req.guild.welcome_screen.welcome_channels, }; else req.guild.welcome_scree = { description: null, WLCChannels: null }
 
             resolve({
@@ -22,7 +22,7 @@ exports.GUILD = ({ url }) => {
                 guild: { id: req.guild.id, name: req.guild.name, memberCount: req.approximate_member_count, presenceCount: req.approximate_presence_count, description: req.guild.description, splash: req.guild.splash, banner: req.guild.banner, icon: req.guild.icon, verificationLVL: req.guild.verification_level, vanityURL: req.guild.vanity_url_code, boostCount: req.guild.premium_subscription_count, nsfw: { NSFW: req.guild.nsfw, NSFWlvl: req.guild.nsfw_level }, wlcScreen: req.guild.welcome_screen, features: req.guild.features }
             })
 
-        }).catch(reject);
+        }).catch(e => { console.log(e.response.data.message || e) });
     });
 
 };
@@ -36,11 +36,29 @@ exports.USER = ({ id, token }) => {
             'Authorization': 'Bot ' + token,
         }
     }
+
     return new Promise((resolve, reject) => {
         axios(config).then(data => {
             const req = data.data;
-            resolve(req)
-        }).catch(reject);
+            if (req.avatar !== null) req.avatar = {
+                png: Settings.User.AVATAR + req.avatar + '.png?size=2048',
+                jpg: Settings.User.AVATAR + req.avatar + '.jpg?size=2048',
+                jpeg: Settings.User.AVATAR + req.avatar + '.jpeg?size=2048',
+                webp: Settings.User.AVATAR + req.avatar + '.webp?size=2048',
+                gif: Settings.User.AVATAR + req.avatar + '.gif?size=2048',
+            }
+            resolve({
+                id: req.id,
+                username: req.username,
+                discriminator: req.disdiscriminator,
+                tag: req.username + '#' + req.disdiscriminator,
+                avatar: req.avatar,
+                // badges: ,
+                // banner: ,
+                // bannerColor: ,
+                // accentColor: 
+            })
+        }).catch(e => { console.log(e.response.data.message || e) });
     });
 
 }
