09569ce39c7a7e14191b2336ce8e81fed98653c5
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-03 00:04:46 +0330

    Update handler.js

--- src/handler.js
+++ src/handler.js
@@ -1,5 +1,7 @@
 exports.Invite = require('./components/Invite.module');
 exports.User = require('./components/User.module');
 exports.Guild = require('./components/Guild.module');
-exports.GuildRoles = require('./components/GuildRoles.module')
-exports.GuildEmojis = require('./components/GuildEmojis.module')
+exports.GuildRoles = require('./components/GuildRoles.module');
+exports.GuildEmojis = require('./components/GuildEmojis.module');
+exports.AccountSettings = require('./components/AccountSettings.module')
+
