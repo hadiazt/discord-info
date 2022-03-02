02f0f9c2555d1ad82d927a1ecc90e725b6921ad3
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 23:03:20 +0330

    Update Invite.module.js

--- src/components/Invite.module.js
+++ src/components/Invite.module.js
@@ -16,11 +16,7 @@ module.exports = ({ url }) => {
             if (req.guild.icon !== null) req.guild.icon = { png: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.png?size=2048', jpg: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.jpg?size=2048', jpeg: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.jpeg?size=2048', webp: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.webp?size=2048', gif: Settings.Invite.ICON + req.guild.id + '/' + req.guild.icon + '.gif?size=2048', }; else req.guild.icon = { png: null, jpg: null, jpeg: null, webp: null, gif: null, }
             if (req.guild.welcome_screen) req.guild.welcome_screen = { description: req.guild.welcome_screen.description, WLCChannels: req.guild.welcome_screen.welcome_channels, }; else req.guild.welcome_scree = { description: null, WLCChannels: null }
 
-            resolve({
-                inv: { invCode: req.code, invType: req.type, invExpireTime: req.expires_at, Channel: { id: req.channel.id, name: req.channel.name, type: req.channel.type } },
-                guild: { id: req.guild.id, name: req.guild.name, memberCount: req.approximate_member_count, presenceCount: req.approximate_presence_count, description: req.guild.description, splash: req.guild.splash, banner: req.guild.banner, icon: req.guild.icon, verificationLVL: req.guild.verification_level, vanityURL: req.guild.vanity_url_code, boostCount: req.guild.premium_subscription_count, nsfw: { NSFW: req.guild.nsfw, NSFWlvl: req.guild.nsfw_level }, wlcScreen: req.guild.welcome_screen, features: req.guild.features }
-            })
-
+            resolve({ inv: { invCode: req.code, invType: req.type, invExpireTime: req.expires_at, Channel: { id: req.channel.id, name: req.channel.name, type: req.channel.type } }, guild: { id: req.guild.id, name: req.guild.name, memberCount: req.approximate_member_count, presenceCount: req.approximate_presence_count, description: req.guild.description, splash: req.guild.splash, banner: req.guild.banner, icon: req.guild.icon, verificationLVL: req.guild.verification_level, vanityURL: req.guild.vanity_url_code, boostCount: req.guild.premium_subscription_count, nsfw: { NSFW: req.guild.nsfw, NSFWlvl: req.guild.nsfw_level }, wlcScreen: req.guild.welcome_screen, features: req.guild.features } })
         }).catch(e => { console.log(e?.response?.data?.message || e) });
     });
 };
