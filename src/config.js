5f43311823494af1c2c63a699f742ebb14ab6d85
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-02 15:51:19 +0330

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,11 +1,18 @@
 exports.Settings = {
-    Guild: {
+    Invite: {
         API: "https://discord.com/api/v10/invites/",
         SPLASH: "https://cdn.discordapp.com/splashes/",
         BANNER: "https://cdn.discordapp.com/banner/",
         ICON: "https://cdn.discordapp.com/icons/"
     },
     User: {
-        API : "https://discord.com/api/v10/users/",
+        API: "https://discord.com/api/v10/users/",
+        SPLASH: "https://cdn.discordapp.com/splashes/",
+        BANNER: "https://cdn.discordapp.com/banner/",
+        ICON: "https://cdn.discordapp.com/icons/"
+    },
+    Guild:{
+        API:"https://discord.com/api/v10/guilds/",
+        ICON: "https://cdn.discordapp.com/icons/"
     }
 }
