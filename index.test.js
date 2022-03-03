7f22e8b37403aa7a6574a594720c2fcc2cda5f08
hadi <65118892+hadiazt@users.noreply.github.com>
2022-03-03 14:10:57 +0330

    Update index.test.js

--- index.test.js
+++ index.test.js
@@ -1,4 +1,13 @@
-const { Invite, User, Guild, GuildRoles, GuildEmojis, Account , AccountSettings , AccountGuilds} = require('./src/handler.js')
+const {
+    Invite,
+    User,
+    Guild,
+    GuildRoles,
+    GuildEmojis,
+    Account,
+    AccountSettings,
+    AccountGuilds,
+} = require('discord-info')
 
 Invite({ url: '' }).then((response) => { console.log(response) })
 
@@ -16,3 +25,38 @@ AccountSettings({ UserToken: '' }).then((response) => { console.log(response) })
 
 AccountGuilds({ UserToken: '' }).then((response) => { console.log(response) })
 
+// --------- OR --------- //
+
+const INFO /* Or Every Var You Want */ = require('discord-info')
+
+
+INFO.Invite({ url: '' }).then((response) => { console.log(response) })
+
+INFO.User({ UserID: '', BotToken: '' }).then((response) => { console.log(response) })
+
+INFO.Guild({ GuildID: '', BotToken: '' }).then((response) => { console.log(response) })
+
+INFO.GuildRoles({ GuildID: '', BotToken: '' }).then((response) => { console.log(response) })
+
+INFO.GuildEmojis({ GuildID: '', BotToken: '' }).then((response) => { console.log(response) })
+
+INFO.Account({ UserToken: '' }).then((response) => { console.log(response) })
+
+INFO.AccountSettings({ UserToken: '' }).then((response) => { console.log(response) })
+
+INFO.AccountGuilds({ UserToken: '' }).then((response) => { console.log(response) })
+
+
+/************************************
+    
+    * Available function are : 👇
+    * Invite (Required Valid Invite Code) 
+    * User (Required UserID & BotToken) 
+    * Guild (Required GuildID & BotToken) 
+    * GuildRoles (Required GuildID & BotToken) 
+    * GuildEmojis (Required GuildID & BotToken) 
+    * Account (Required UserToken) 
+    * AccountSettings (Required UserToken) 
+    * AccountGuilds (Required UserToken) 
+    
+************************************/
